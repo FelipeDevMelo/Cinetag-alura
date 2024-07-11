fcb48c6146d4fe04a0047d662489f542aca19c3b
Felipe <luizfelipemelo001@gmail.com>
2024-07-11 16:14:47 -0300

    :sparkles: Feat: Footer component

---
+++ src/Components/Robape/index.js
@@ -0,0 +1,8 @@
+import style from "./Rodabe.module.css";
+export const Rodape = () => {
+  return (
+    <footer className={style.rodape}>
+      <p>Desenvolvido por Felipe</p>
+    </footer>
+  );
+};
--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,7 +1,13 @@
 import Cabecalho from "Components/Cabecalho";
+import { Rodape } from "Components/Robape";
 
 const Inicio = () => {
-  return <Cabecalho />;
+  return (
+    <>
+      <Cabecalho />;
+      <Rodape />
+    </>
+  );
 };
 
 export default Inicio;
