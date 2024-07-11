ba3cad99ea36eecddf60f9ec3e29af614ff4c3d7
Felipe <luizfelipemelo001@gmail.com>
2024-07-11 17:06:44 -0300

    sparkles: Feat: titulo component

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,12 +1,16 @@
 import { Banner } from "Components/Banner";
 import Cabecalho from "Components/Cabecalho";
 import { Rodape } from "Components/Robape";
+import { Titulo } from "Components/Titulo";
 
 const Inicio = () => {
   return (
     <>
       <Cabecalho />
       <Banner imagem="home" />
+      <Titulo>
+        <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+      </Titulo>
       <Rodape />
     </>
   );
